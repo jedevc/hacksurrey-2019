f9aafc9c8f1ec0f6af85c05b19fd3e59dd06e33d
wrussell1999 <jetpowerss@gmail.com>
2019-02-09 20:28:59 +0000

    Guardtime SDK added

--- android-app/app/src/main/java/com/will_russell/hacksurrey/MainActivity.java
+++ android-app/app/src/main/java/com/will_russell/hacksurrey/MainActivity.java
@@ -13,16 +13,68 @@ import android.content.pm.PackageManager;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.guardtime.ksi.KSI;
+import com.guardtime.ksi.KSIBuilder;
+import com.guardtime.ksi.exceptions.KSIException;
+import com.guardtime.ksi.hashing.DataHash;
+import com.guardtime.ksi.hashing.DataHasher;
+import com.guardtime.ksi.service.client.KSIServiceCredentials;
 import com.guardtime.ksi.service.client.http.HttpClientSettings;
+import com.guardtime.ksi.service.http.simple.SimpleHttpClient;
+import com.guardtime.ksi.trust.X509CertificateSubjectRdnSelector;
+import com.guardtime.ksi.unisignature.KSISignature;
+import com.guardtime.ksi.unisignature.verifier.policies.KeyBasedVerificationPolicy;
+import com.guardtime.ksi.unisignature.verifier.policies.Policy;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int GET_FROM_STORAGE = 1;
-
+    ArrayList<Uri> files = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        KSIServiceCredentials credentials = new KSIServiceCredentials("ot.9HL3ao", "F8ByaJsVcq0c");
+
+        HttpClientSettings clientSettings = new HttpClientSettings(
+                "http://tryout.guardtime.net:8080/gt-signingservice",
+                "https://tryout-extender.guardtime.net:8081/gt-extendingservice",
+                "http://verify.guardtime.com/ksi-publications.bin",
+                credentials);
+
+        SimpleHttpClient simpleHttpClient = new SimpleHttpClient(clientSettings);
+        try {
+            KSI ksi = new KSIBuilder()
+                    .setKsiProtocolSignerClient(simpleHttpClient)
+                    .setKsiProtocolExtenderClient(simpleHttpClient)
+                    .setKsiProtocolPublicationsFileClient(simpleHttpClient)
+                    .setPublicationsFileTrustedCertSelector(new X509CertificateSubjectRdnSelector("E=publications@guardtime.com"))
+                    .build();
+        } catch(Exception e) {
+            
+        }
+
+        DataHasher hasher = new DataHasher();
+
+        // Read the input file to be signed
+        FileInputStream inputStream;
+        byte[] fileContent;
+        try {
+            inputStream = new FileInputStream("signme.txt"); // CHANGE TO ANDROID INPUT
+            fileContent = Files.readAllBytes(Paths.get("signme.txt")); // CHANGE TO ANDROID
+            System.out.printf("\n\nInput file: %s\n", new String(fileContent));
+            hasher.addData(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void uploadButtonOnClick(View v) {
@@ -51,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (requestCode == GET_FROM_STORAGE && resultCode == Activity.RESULT_OK) {
             Uri fileUri = data.getData();
+            files.add(fileUri);
         }
     }
 }
