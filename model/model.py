3e7d5a15b10c14b30a6f76f8b845731c6f7523e7
Justin Chadwell <jedevc@gmail.com>
2019-02-09 21:57:57 +0000

    Use permutations in actual model generation code :)

--- model/model.py
+++ model/model.py
@@ -7,8 +7,8 @@ import random
 from . import permute
 
 def main():
-    order = [1, 2, 3, 4, 5, 6]
-    random.shuffle(order)
+    order = permute.permute([1, 2, 3, 4, 5, 6], random.randint(0, 719))
+
     heights = [0.5 + 1.5 * i / len(order) for i in order]
     st = stump(heights)
 
