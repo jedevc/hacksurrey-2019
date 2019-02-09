b122a795c6d3d7a77042196a817c44468eacae71
Justin Chadwell <jedevc@gmail.com>
2019-02-09 22:22:00 +0000

    Use random fake hash data to generate a stump

--- model/model.py
+++ model/model.py
@@ -7,10 +7,27 @@ import random
 from . import permute
 
 def main():
-    order = permute.permute([1, 2, 3, 4, 5, 6], random.randint(0, 719))
+    stuff = bytes(random.randint(0, 255) for i in range(32))
+    stuff = int.from_bytes(stuff, 'big')
+    hexes = to_base(stuff, 720)
+
+    order = permute.permute([1, 2, 3, 4, 5, 6], hexes[0])
 
     heights = [0.5 + 1.5 * i / len(order) for i in order]
     st = stump(heights)
 
     scad = scad_render(st)
     print(scad)
+
+def to_base(number, base):
+    top = 1
+    while base ** top < number:
+        top += 1
+
+    parts = []
+    for i in range(top - 1, -1, -1):
+        value = base ** i
+        parts.append(number // value)
+        number %= value
+
+    return parts
