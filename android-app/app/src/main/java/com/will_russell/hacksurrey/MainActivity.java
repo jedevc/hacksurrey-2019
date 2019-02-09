da179cc230ce9bca0a1ce1dad5b6222ec901f691
wrussell1999 <jetpowerss@gmail.com>
2019-02-09 16:02:45 +0000

    Upload button

--- android-app/app/src/main/java/com/will_russell/hacksurrey/MainActivity.java
+++ android-app/app/src/main/java/com/will_russell/hacksurrey/MainActivity.java
@@ -2,13 +2,36 @@ package com.will_russell.hacksurrey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+import com.guardtime.ksi.service.client.http.HttpClientSettings;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int GET_FROM_STORAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void uploadButtonOnClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, GET_FROM_STORAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GET_FROM_STORAGE && resultCode == Activity.RESULT_OK) {
+            Uri fileUri = data.getData();
+        }
     }
 }
