6b842533532a1f00d47d3a97a98c07f8a6bfb15a
Justin Chadwell <jedevc@gmail.com>
2019-02-09 13:13:43 +0000

    Use fun hashing of data

--- server/server.py
+++ server/server.py
@@ -1,11 +1,36 @@
 import flask
-from flask import Flask
+from flask import Flask, request
+
+import hashlib
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello World!'
+
+@app.route('/create', methods=['POST'])
+def create():
+    data = None
+    if 'file' in request.form:
+        data = request.form['file'].encode('utf8')
+    elif 'file' in request.files:
+        datafile = request.files.get('file')
+        if datafile:
+            data = datafile.read()
+
+    if not data:
+        return 'invalid request', 400
+
+    h = hashlib.sha256()
+    h.update(data)
+    hid = h.hexdigest()
+
+    return hid
+
+@app.errorhandler(404)
+def not_found(error):
+    return 'endpoint not found', 404
 
 def main():
     app.run()
