947ed5744db75ae008e75c37dff747815d095bc6
wrussell1999 <jetpowerss@gmail.com>
2019-02-09 18:37:24 +0000

    Storage permissions in the works

--- android-app/app/src/main/java/com/will_russell/hacksurrey/MainActivity.java
+++ android-app/app/src/main/java/com/will_russell/hacksurrey/MainActivity.java
@@ -7,6 +7,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.guardtime.ksi.service.client.http.HttpClientSettings;
 
@@ -21,9 +26,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void uploadButtonOnClick(View v) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        startActivityForResult(intent, GET_FROM_STORAGE);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("Permission denied");
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                System.out.println("Rip");
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, GET_FROM_STORAGE);
+            }
+        } else {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            startActivityForResult(intent, GET_FROM_STORAGE);
+        }
     }
 
     @Override
@@ -32,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (requestCode == GET_FROM_STORAGE && resultCode == Activity.RESULT_OK) {
             Uri fileUri = data.getData();
+            System.out.println("Wow!");
         }
     }
 }
