c8561c252af83727fbccbd2859dda187b0bf83b3
Justin Chadwell <jedevc@gmail.com>
2019-02-09 13:23:42 +0000

    Actually add file storage instead of just faking it :P

--- server/server.py
+++ server/server.py
@@ -1,9 +1,14 @@
 import flask
 from flask import Flask, request
 
+import os
+from os import path
 import hashlib
+import re
 
 app = Flask(__name__)
+
+UPLOADS = '/tmp/uploads'
 
 @app.route('/')
 def hello():
@@ -26,15 +31,29 @@ def create():
     h.update(data)
     hid = h.hexdigest()
 
+    filename = path.join(UPLOADS, hid)
+    with open(filename, 'wb') as f:
+        f.write(data)
+
     return hid
 
 @app.route('/file/<hid>')
 def get_file(hid):
-    return 'this is a file! ' + hid
+    if re.search('[^0-9a-f]', hid):
+        return 'invalid hash provided'
+
+    filename = path.join(UPLOADS, hid)
+    try:
+        with open(filename) as f:
+            return f.read()
+    except FileNotFoundError:
+        return 'no file with that hash exists', 404
 
 @app.errorhandler(404)
 def not_found(error):
     return 'endpoint not found', 404
 
 def main():
+    os.makedirs(UPLOADS, exist_ok=True)
+
     app.run()
