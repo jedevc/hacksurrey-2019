8cf988738f3180ef7c2cdf99e1985810615f1ac5
Justin Chadwell <jedevc@gmail.com>
2019-02-10 11:33:14 +0000

    Add model stuffs

--- hexblock/model/model.py
+++ hexblock/model/model.py
@@ -9,8 +9,30 @@ from . import permute
 
 def main():
     stuff = hashlib.sha256(b'data').digest()
+    final = create_model(stuff, 3, 9)
 
-    m = create_model(stuff, 3, 9)
+    print(render_model(final))
+
+def create_model(data, rows, columns):
+    hexes = to_base(int.from_bytes(data, 'big'), 720)
+    assert len(hexes) == rows * columns
+
+    sts = []
+    for row in range(rows):
+        for col in range(columns):
+            order = permute.permute([1, 2, 3, 4, 5, 6], hexes[row * 9 + col])
+
+            heights = [1 + 1.5 * i / len(order) for i in order]
+            st = stump(heights)
+
+            xdiff = 1.5 * col
+            xdiff *= 1.2
+            ydiff = (-3 ** 0.5) * row - (3 ** 0.5 / 2) * col
+            ydiff *= 1.2
+            st = solid.translate([xdiff, ydiff, 0])(st)
+
+            sts.append(st)
+
     base = solid.translate([-3.4, -3.2, 0])(
         solid.rotate([0, 0, -30])(
             solid.scale([22, 6.5, 0.3])(
@@ -39,30 +61,13 @@ def main():
         )
     )
 
-    final = solid.union()([base, m, marker])
-    print(render_model(final))
-
-def create_model(data, rows, columns):
-    hexes = to_base(int.from_bytes(data, 'big'), 720)
-    assert len(hexes) == rows * columns
-
-    sts = []
-    for row in range(rows):
-        for col in range(columns):
-            order = permute.permute([1, 2, 3, 4, 5, 6], hexes[row * 9 + col])
-
-            heights = [1 + 1.5 * i / len(order) for i in order]
-            st = stump(heights)
-
-            xdiff = 1.5 * col
-            xdiff *= 1.2
-            ydiff = (-3 ** 0.5) * row - (3 ** 0.5 / 2) * col
-            ydiff *= 1.2
-            st = solid.translate([xdiff, ydiff, 0])(st)
-
-            sts.append(st)
-
-    return solid.union()(sts)
+    return solid.union()(
+        [
+            base,
+            solid.union()(sts),
+            marker
+        ]
+    )
 
 def render_model(model):
     return solid.scad_render(model)
